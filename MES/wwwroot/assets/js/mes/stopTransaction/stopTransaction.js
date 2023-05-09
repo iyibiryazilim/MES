842a2ffbee3652192e554eb21b714530b6683940
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-05-09 13:12:35 +0300

    employee list fix. StopTransaction Added

--- MES/wwwroot/assets/js/mes/stopTransaction/stopTransaction.js
+++ MES/wwwroot/assets/js/mes/stopTransaction/stopTransaction.js
@@ -4,7 +4,7 @@
 
 // Class definition
 
-var EmployeeList = function () {
+var StopTransactionList = function () {
 
     // Shared variables
     var table;
@@ -15,9 +15,10 @@ var EmployeeList = function () {
 
     var initDatatable = function () {
 
-        var postUrl = '/Employee/GetJsonResult';
+        var postUrl = '/StopTransaction/GetJsonResult';
 
         datatable = $(table).DataTable({
+
             responsive: true,
             autoWidth: false,
             searchDelay: 500,
@@ -32,14 +33,21 @@ var EmployeeList = function () {
             columns: [
 
                 { data: 'referenceId' },
-                { data: 'name' },
-                { data: 'code' },
-                { data: 'shiftProgress' },
+                { data: 'description' },
+                { data: 'operation' },
+                { data: 'productionOrder' },
+                { data: 'workOrder' },
+                { data: 'workstation' },
+                { data: 'stopCause' },
+                { data: 'stopDate' },
+                { data: 'stopTime' },
+                { data: 'stopDuration' },
+                { data: 'startDate' },
+                { data: 'startTime' },
                 { data: 'referenceId' },
 
 
             ],
-
             columnDefs: [
                 {
                     orderable: true,
@@ -49,39 +57,29 @@ var EmployeeList = function () {
                         var output;
 
                         output = `<div class="form-check form-check-sm form-check-custom form-check-solid">
-                            <input class="form-check-input" type="checkbox" value="`+ data + `" />
+                            <input class="form-check-input" type="checkbox" value="`+ full.referenceId + `" />
                         </div>`
                         return output;
 
                     },
 
                 },
+
+
                 {
 
                     orderable: true,
                     targets: 1,
+                    className: 'text-start pe-0',
                     render: function (data, type, full, meta) {
 
                         var output;
-
-                        output = `<div class="d-flex align-items-center">
-																<!--begin::Thumbnail-->
-										<a href="../../demo46/dist/apps/ecommerce/catalog/edit-product.html" class="symbol symbol-50px">
-											<span class="symbol-label" style="background-image:url(assets/media//stock/ecommerce/1.gif);"></span>
-										</a>
-										<!--end::Thumbnail-->
-										<div class="ms-5">
-											<!--begin::Title-->
-											<a href="../../demo46/dist/apps/ecommerce/catalog/edit-product.html" class="text-gray-800 text-hover-primary fs-5 fw-bold" data-kt-ecommerce-product-filter="product_name">`+ data + `</a>
-											<!--end::Title-->
-																</div>
-															</div>`
+                        output = `<div class="fw-bold">` + full.description + `</div>`
                         return output;
 
                     },
 
                 },
-               
                 {
 
                     orderable: true,
@@ -90,7 +88,7 @@ var EmployeeList = function () {
                     render: function (data, type, full, meta) {
 
                         var output;
-                        output = `<div class="text-gray-800 text-hover-primary mb-1">` + full.code + `</div>`
+                        output = `<div class="fw-bold">` + full.operation.name + `</div>`
                         return output;
 
                     },
@@ -103,28 +101,43 @@ var EmployeeList = function () {
                     className: 'text-start pe-0',
                     render: function (data, type, full, meta) {
 
+                        var output;
+                        output = `<div class="fw-bold">` + full.productionOrder.product.name + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 4,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
 
                         var output;
-                        if (full.shiftProgress <= 33 && full.shiftProgress >= 0) {
-                            output = `<div class="text-muted fs-7 fw-bold">` + "%" + full.shiftProgress + `</div>
-                                    <div class="h-8px mx-3 w-100 bg-light-danger rounded">
-									<div class="bg-danger rounded h-8px" role="progressbar" style="width: `+ full.shiftProgress + `%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-								  </div>`
-                            return output;
-                        } else if (full.shiftProgress > 33 && full.shiftProgress <= 66) {
-                            output = `<div class="text-muted fs-7 fw-bold">` + "%" + full.shiftProgress + `</div>
-                                    <div class="h-8px mx-3 w-100 bg-light-warning rounded">
-									<div class="bg-warning rounded h-8px" role="progressbar" style="width: `+ full.shiftProgress + `%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-								  </div>`
-                            return output;
-
-                        }
-                        else {
-                            output = `<div class="text-muted fs-7 fw-bold">` + "%" + full.shiftProgress + `</div>
-                                    <div class="h-8px mx-3 w-100 bg-light-primary rounded">
-									<div class="bg-primary rounded h-8px" role="progressbar" style="width: `+ full.shiftProgress + `%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-								  </div>`
-                            return output;
+                        switch (full.workOrder.status) {
+                            case 0:
+                                output = `<div class="badge badge-light-danger fw-bold">` + "Başlamadı " + `</div>`
+                                return output;
+                                break;
+                            case 1:
+                                output = `<div class="badge badge-light-warning fw-bold">` + "Devam Ediyor " + `</div>`
+                                return output;
+                                break;
+                            case 2:
+                                output = `<div class="badge badge-light fw-bold">` + "Statü " + full.workOrder.status + `</div>`
+                                return output;
+                                break;
+                            case 3:
+                                output = `<div class="badge badge-light fw-bold">` + "Statü " + full.workOrder.status + `</div>`
+                                return output;
+                                break;
+                            case 4:
+                                output = `<div class="badge badge-light-primary fw-bold">` + "Kapanmış " + `</div>`
+                                return output;
+                                break;
+                            default:
                         }
 
                     },
@@ -132,14 +145,112 @@ var EmployeeList = function () {
                 },
                 {
 
+                    orderable: true,
+                    targets: 5,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.workstation.name + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 6,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.stopCause.name + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 7,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.stopDate + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 8,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.stopTime + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 9,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.stopDuration + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 10,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.startDate + `</div>`
+                        return output;
+
+                    },
+
+                },
+                {
+
+                    orderable: true,
+                    targets: 11,
+                    className: 'text-start pe-0',
+                    render: function (data, type, full, meta) {
+
+                        var output;
+                        output = `<div class="fw-bold">` + full.startTime + `</div>`
+                        return output;
+
+                    },
+
+                },
+                
+
+                {
+
                     orderable: false,
-                    targets: 4,
+                    targets: 12,
                     className: 'text-end',
                     render: function (data, type, full, meta) {
                         var output;
-                        output = `<a href="#" class="btn btn-sm btn-light btn-active-light-primary btn-flex btn-center" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-							İşlemler
-							<i class="ki-duotone ki-down fs-5 ms-1"></i>
+                        output = `<a href="#" class="btn btn-sm btn-light btn-active-light-primary btn-flex btn-center" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">İşlemler<i class="ki-duotone ki-down fs-5 ms-1"></i>
 						</a>
 						<!--begin::Menu-->
 						<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
@@ -336,17 +447,17 @@ var EmployeeList = function () {
     return {
 
         init: function () {
-            table = document.querySelector('#mes_employee_table');
+            table = document.querySelector('#mes_stopTransaction_table');
 
             if (!table) {
-
+                console.log("table bulunamadı")
                 return;
 
             }
 
             initDatatable();
             handleSearchDatatable();
-            // handleStatusFilter();
+            handleStatusFilter();
             //handleDeleteRows();
         }
 
@@ -359,5 +470,5 @@ var EmployeeList = function () {
 // On document ready
 
 KTUtil.onDOMContentLoaded(function () {
-    EmployeeList.init();
+   StopTransactionList.init();
 });
