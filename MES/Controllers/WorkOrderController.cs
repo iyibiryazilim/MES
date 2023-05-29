c72d4faaa2dc33c136259a7969b16fd897bae366
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-05-29 12:30:08 +0300

    İş istasyonu ve iş emirleri düzenlendi

--- MES/Controllers/WorkOrderController.cs
+++ MES/Controllers/WorkOrderController.cs
@@ -1,6 +1,7 @@
 ﻿using LBS.WebAPI.Service.Services;
 using MES.HttpClientService;
 using MES.Models.WorkOrderModels;
+using MES.Models.WorkstationModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
@@ -11,15 +12,15 @@ public class WorkOrderController : Controller
     readonly ILogger<WorkOrderController> _logger;
     readonly IHttpClientService _httpClientService;
     readonly IWorkOrderService _workOrderService;
-	readonly ICustomQueryService _customQueryService;
+    readonly ICustomQueryService _customQueryService;
     public WorkOrderController(ILogger<WorkOrderController> logger,
         IHttpClientService httpClientService,
-        IWorkOrderService workOrderService,ICustomQueryService customQueryService)
+        IWorkOrderService workOrderService, ICustomQueryService customQueryService)
     {
         _logger = logger;
         _httpClientService = httpClientService;
         _workOrderService = workOrderService;
-		_customQueryService = customQueryService;
+        _customQueryService = customQueryService;
     }
     public IActionResult Index()
     {
@@ -36,120 +37,42 @@ public class WorkOrderController : Controller
     private async IAsyncEnumerable<WorkOrderListModel> GetWorkOrder()
     {
         WorkOrderListModel viewModel = new WorkOrderListModel();
-		HttpClient httpClient = _httpClientService.GetOrCreateHttpClient();
-		if (viewModel != null)
-		{
-			string query = $@"SELECT WORKSTATION.NAME AS[WORKSTATIONNAME]
-							 , WORKSTATION.CODE AS [WORKSTATIONCODE]
-                             , WORKORDER.WSREF AS[WORKSTATIONID]
-                             , ITEMS.NAME AS [PRODUCTNAME]
-							 , ITEMS.CODE AS [PRODUCTCODE]
-                             , WORKORDER.ITEMREF AS[PRODUCTID]
+        HttpClient httpClient = _httpClientService.GetOrCreateHttpClient();
+        if (viewModel != null)
+        {
+            string query = $@"SELECT WORKSTATION.NAME AS[WorkstationName]
+							 , WORKSTATION.CODE AS [WorkstationCode]
+                             , WORKORDER.WSREF AS[WorkstationId]
+                             , ITEMS.NAME AS [ProductName]
+							 , ITEMS.CODE AS [ProductCode]
+                             , WORKORDER.ITEMREF AS [ProductId]
                              , WORKORDER.LINESTATUS AS[WORKORDERSTATUS]
-                             , WORKORDER.LOGICALREF AS[REFERENCEID]
-                             , WORKORDER.OPBEGDATE AS[PLANNEDBEGIN]
-                             , OPERATION.LOGICALREF AS[OPERATIONID]
-                             , OPERATION.NAME AS [OPERATIONNAME]
-                             , WORKORDER.OPDUEDATE AS [PLANNEDDUE]
-                             , PRODUCTIONORDER.PLNAMOUNT AS[PLANNEDAMOUNTH]
+                             , WORKORDER.LOGICALREF AS[ReferenceId]
+                             , WORKORDER.OPBEGDATE AS[PlannedBeginDate]
+                             , OPERATION.LOGICALREF AS[OperationId]
+                             , OPERATION.NAME AS [OperationName]
+                             , WORKORDER.OPDUEDATE AS [PlannedDueDate]
+                             , PRODUCTIONORDER.PLNAMOUNT AS[PlannedAmounth]
                              ,[ActualAmounth] = (SELECT ISNULL(SUM(AMOUNT), 0) FROM LG_003_01_STLINE WHERE POLINEREF = WORKORDER.LOGICALREF AND TRCODE = 13 AND LPRODSTAT = 1)
 							 
                              FROM LG_003_DISPLINE AS WORKORDER LEFT JOIN LG_003_WORKSTAT AS WORKSTATION ON WORKORDER.WSREF = WORKSTATION.LOGICALREF
                              LEFT JOIN LG_003_ITEMS AS ITEMS ON WORKORDER.ITEMREF = ITEMS.LOGICALREF
                              LEFT JOIN LG_003_OPERTION AS OPERATION ON WORKORDER.OPERATIONREF = OPERATION.LOGICALREF
                              LEFT JOIN LG_003_PRODORD AS PRODUCTIONORDER ON WORKORDER.PRODORDREF = PRODUCTIONORDER.LOGICALREF";
-			JsonDocument? jsonDocument = await _customQueryService.GetObjects(httpClient, query);
-			if (jsonDocument != null)
-			{
-				var array = jsonDocument.RootElement.EnumerateArray();
-				foreach (JsonElement element in array)
-				{
-					#region Reference Id
-					JsonElement referenceId = element.GetProperty("referenceid");
-					viewModel.ReferenceId = Convert.ToInt32(referenceId.GetRawText().Replace('.', ','));
-					#endregion
+            JsonDocument? jsonDocument = await _customQueryService.GetObjects(httpClient, query);
+            if (jsonDocument != null)
+            {
+                List<WorkOrderListModel> result = (List<WorkOrderListModel>)jsonDocument.Deserialize(typeof(List<WorkOrderListModel>), new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                if (result != null)
+                {
+                    foreach (WorkOrderListModel item in result)
+                    {
 
-					#region Product Name
-					JsonElement productName = element.GetProperty("productname");
-					viewModel.ProductName = productName.GetString();
-					#endregion
-
-					#region Product Code
-					JsonElement productCode = element.GetProperty("productcode");
-					viewModel.ProductCode = productCode.GetString();
-					#endregion
-
-					#region Product Id
-					JsonElement productId = element.GetProperty("productid");
-					viewModel.ProductId = Convert.ToInt32(productId.GetRawText().Replace('.', ','));
-					#endregion
-
-					#region Workstation Name 
-					JsonElement workstationName = element.GetProperty("workstationname");
-					viewModel.WorkstationName = workstationName.GetString();
-					#endregion
-
-					#region Workstation Id
-					JsonElement workstationId = element.GetProperty("workstationid");
-					viewModel.WorkstationId = Convert.ToInt32(workstationId.GetRawText().Replace('.', ','));
-					#endregion
-
-					#region Workstation Code 
-					JsonElement workstationCode = element.GetProperty("workstationcode");
-					viewModel.WorkstationCode = workstationCode.GetString();
-					#endregion
-
-					#region Work Order Status
-					JsonElement workOrderStatus = element.GetProperty("workorderstatus");
-					viewModel.WorkOrderStatus = Convert.ToInt32(workOrderStatus.GetRawText().Replace('.', ','));
-					#endregion
-
-					#region Operation Id 
-					JsonElement operationId = element.GetProperty("operationid");
-					viewModel.OperationId = Convert.ToInt32(operationId.GetRawText().Replace('.', ','));
-					#endregion
-
-					#region  Operation Name
-					JsonElement operationName = element.GetProperty("operationname");
-					viewModel.OperationName = operationName.GetString();
-					#endregion
-
-					#region Planned Begin
-					JsonElement plannedBeginDate = element.GetProperty("plannedbegin");
-					if (element.TryGetProperty("plannedbegin", out plannedBeginDate) && plannedBeginDate.ValueKind != JsonValueKind.Null)
-					{
-						viewModel.PlannedBeginDate = JsonSerializer.Deserialize<DateTime>(plannedBeginDate.GetRawText());
-					}
-					#endregion
-
-					#region Planned Due
-					JsonElement plannedDueDate = element.GetProperty("planneddue");
-					if (element.TryGetProperty("planneddue", out plannedDueDate) && plannedDueDate.ValueKind != JsonValueKind.Null)
-					{
-						viewModel.PlannedDueDate = JsonSerializer.Deserialize<DateTime>(plannedDueDate.GetRawText());
-					}
-					#endregion
-
-					#region  Planned Amounth
-					JsonElement plannedAmounth = element.GetProperty("plannedamounth");
-					viewModel.PlannedAmounth = Convert.ToDouble(plannedAmounth.GetRawText().Replace('.', ','));
-					#endregion
-
-					#region  Actual Amounth
-					JsonElement actualAmounth = element.GetProperty("actualAmounth");
-					viewModel.ActualAmounth = Convert.ToDouble(actualAmounth.GetRawText().Replace('.', ','));
-					#endregion
-
-
-					//Console.WriteLine(Convert.ToInt64(100 * (viewModel.ActualAmounth / viewModel.PlannedAmounth)).ToString());
-					#region Realization Rate
-					viewModel.RealizationRate = Convert.ToInt64(100*(viewModel.ActualAmounth / viewModel.PlannedAmounth));
-					#endregion
-
-					yield return viewModel;
-				}
-			}
-		}
-	}
+                        yield return item;
+                    }
+                }
+            }
+        }
+    }
 }
 
--- MES/Controllers/WorkStationGroupController.cs
+++ MES/Controllers/WorkStationGroupController.cs
@@ -2,6 +2,7 @@
 using MES.HttpClientService;
 using MES.Models.StopCauseModels;
 using MES.Models.WorkstationGroupModels;
+using MES.Models.WorkstationModels;
 using MES.ViewModels.WorkStationGroupViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
@@ -10,33 +11,33 @@ namespace MES.Controllers;
 
 public class WorkStationGroupController : Controller
 {
-	readonly ILogger<WorkStationGroupController> _logger;
-	readonly IHttpClientService _httpClientService;
-	readonly IWorkstationGroupService _workstationGroupService;
+    readonly ILogger<WorkStationGroupController> _logger;
+    readonly IHttpClientService _httpClientService;
+    readonly IWorkstationGroupService _workstationGroupService;
     readonly ICustomQueryService _customQueryService;
-	public WorkStationGroupController(ILogger<WorkStationGroupController> logger,
-		IHttpClientService httpClientService,
-		IWorkstationGroupService workstationGroupService,ICustomQueryService customQueryService)
-	{
-		_logger = logger;
-		_httpClientService = httpClientService;
-		_workstationGroupService = workstationGroupService;
+    public WorkStationGroupController(ILogger<WorkStationGroupController> logger,
+        IHttpClientService httpClientService,
+        IWorkstationGroupService workstationGroupService, ICustomQueryService customQueryService)
+    {
+        _logger = logger;
+        _httpClientService = httpClientService;
+        _workstationGroupService = workstationGroupService;
         _customQueryService = customQueryService;
-	}
-	public IActionResult Index()
-	{
+    }
+    public IActionResult Index()
+    {
         ViewData["Title"] = "İş İstasyonu Grupları";
         return View();
-	}
+    }
 
-	[HttpPost]
-	public async ValueTask<IActionResult> GetJsonResult()
-	{
-		return Json(new { data = GetWorkstations() });
-	}
+    [HttpPost]
+    public async ValueTask<IActionResult> GetJsonResult()
+    {
+        return Json(new { data = GetWorkstations() });
+    }
 
-	private async IAsyncEnumerable<WorkstationGroupListModel> GetWorkstations()
-	{
+    private async IAsyncEnumerable<WorkstationGroupListModel> GetWorkstations()
+    {
         HttpClient httpClient = _httpClientService.GetOrCreateHttpClient();
         WorkstationGroupListModel viewModel = new();
 
@@ -61,29 +62,14 @@ FROM
             JsonDocument? jsonDocument = await _customQueryService.GetObjects(httpClient, query);
             if (jsonDocument != null)
             {
-                var array = jsonDocument.RootElement.EnumerateArray();
-                foreach (JsonElement element in array)
+                List<WorkstationGroupListModel> result = (List<WorkstationGroupListModel>)jsonDocument.Deserialize(typeof(List<WorkstationGroupListModel>), new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                if (result != null)
                 {
-                    #region Reference Id
-                    JsonElement referenceId = element.GetProperty("referenceid");
-                    viewModel.ReferenceId = Convert.ToInt32(referenceId.GetRawText().Replace('.', ','));
-                    #endregion
+                    foreach (WorkstationGroupListModel item in result)
+                    {
 
-                    #region Code
-                    JsonElement code = element.GetProperty("code");
-                    viewModel.Code = code.GetString();
-                    #endregion
-
-                    #region Description
-                    JsonElement name = element.GetProperty("name");
-                    viewModel.Name = name.GetString();
-                    #endregion
-
-                    #region Workstation Count
-                    JsonElement workstationCount = element.GetProperty("workstationCount");
-                    viewModel.WorkstationCount = Convert.ToInt32(workstationCount.GetRawText().Replace('.', ','));
-                    #endregion
-                    yield return viewModel;
+                        yield return item;
+                    }
                 }
             }
         }
